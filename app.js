9ad3fd7d07a4d5c40d70e8e373df485f47bcdb83
melissabutler <melissaebutler2015@gmail.com>
2023-09-12 09:54:31 -0400

    troubleshooting js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 function randomRGB() {
-    const r = Math.floor(Math.random() *256);
-    const g = Math.floor(Math.random() *256);
-    const b = Math.floor(Math.random() *256);
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
     return `rgb(${r},${g},${b})`
 }
 
 const letters = document.querySelectorAll('.letter');
 const intervalID = setInterval(function(){
     for (let letter of letters) {
-        letter.computedStyleMap.color = randomRGB();
+        letter.style.color = randomRGB();
     }
 }, 1000);
